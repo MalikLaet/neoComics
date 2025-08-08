3d220780a03e5984a3d12a759d98930d7860f025
Malik Laet <isklikma@gmail.com>
2025-08-08 17:57:32 -0300

    consuming mavel api and adding it to the page

--- app/components/Cards/index.tsx
+++ app/components/Cards/index.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { ButtonCard, CardContariner, PriceCard, TittleCard } from "./styled";
-import comicsMock from "@/app/mocks/comicsMock.json";
+// import comicsMock from "@/app/mocks/comicsMock.json";
 import Link from "next/link";
+import md5 from "crypto-js/md5";
 
 export interface Comic {
   id: number;
@@ -13,12 +14,35 @@ export interface Comic {
   };
 }
 
+const publicKey = "cca0fda9ccd59784c51d1235a0d916e4";
+const privateKey = "e2836998fb198bd9fc62356ee436afda64b09c5c";
+
 export default function Cards() {
-  const [comics, setComics] = useState<Comic[]>([]);
+  const ts = Date.now().toString();
+  const hash = md5(ts + privateKey + publicKey).toString();
+  const [comics, setComics] = useState<Comic[]>([]); 
 
   useEffect(() => {
-    setComics(comicsMock);
-  }, []);
+    const fetchComics = async () => {
+      try {
+        const response = await fetch(
+          `https://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+        );
+        if (!response.ok) {
+          throw new Error("Erro na requisição");
+        }
+        const json = await response.json();
+        const filteredComics = json.data.results.filter(
+        (comic) => !comic.thumbnail.path.includes("image_not_available")
+      );
+setComics(filteredComics);
+      } catch (error) {
+        console.error("Erro ao buscar quadrinhos:", error);
+      }
+    };
+
+    fetchComics();
+  }, [ts, hash]);
 
   return (
     <>
@@ -28,7 +52,7 @@ export default function Cards() {
                   <Link key={comic.id} href={`/comics/${comic.id}`}>
           <CardContariner>
               <Image
-                src={`${comic.thumbnail.path}`}
+                src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
                 alt={comic.title}
                 width={150}
                 height={200}
