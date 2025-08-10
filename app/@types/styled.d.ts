fe88e4a9c61b18fffadfbfcc16755ab0118c5002
Malik Laet <isklikma@gmail.com>
2025-08-10 17:22:27 -0300

    resolve bug lint vercel

--- app/@types/styled.d.ts
+++ app/@types/styled.d.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import "styled-components";
 import { defaultTheme } from "../styles/themes/default";
 
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme extends ThemeType {
+    
+  }
 }
