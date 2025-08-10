9dae32dd00a1c54bf87216a8e33c12359b3dd998
Malik Laet <isklikma@gmail.com>
2025-08-10 17:45:03 -0300

    resolve bug layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,17 @@
-'use client'
+import { ReactNode } from "react";
+import { GlobalStyle } from "./styles/global";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-import { Provider } from "react-redux"
-import { store } from "./store"
-import { GlobalStyle } from "./styles/global"
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" >
-      <body >
-         <GlobalStyle />
+    <html lang="en">
+      <body>
+        <GlobalStyle />
         <Provider store={store}>
           {children}
         </Provider>
       </body>
     </html>
-  )
+  );
 }
