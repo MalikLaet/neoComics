a7c48ee2a12b8eb8fd326749f153d7d3e1addac1
Malik Laet <isklikma@gmail.com>
2025-08-10 17:55:40 -0300

    try resolve bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
-import { ReactNode } from "react";
 import { GlobalStyle } from "./styles/global";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-interface RootLayoutProps {
-  children: ReactNode;
-}
-
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
